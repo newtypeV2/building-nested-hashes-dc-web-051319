5b734cfd0e22a87cd1a9a3991a7f3d118791cc9b
Gian Libunao <gianlibunao@gmail.com>
2019-05-05 14:05:30 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -17,7 +17,7 @@ def fourth_challenge
       :heroine_friends => [
         #your hashes here!
          Steven:{name:"Steven",age:"30",attitude:"confused"}
-         Nurse:{name:"Nurse",age:"18",attitude:"hot-headed"}
+         Nurse:{name:"Nurse",age:"44",attitude:"worrioed"}
       ]
    }
 }
