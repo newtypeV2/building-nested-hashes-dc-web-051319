e93c0ea471288e5b56d96381bbe8d4686d2e6683
Gian Libunao <gianlibunao@gmail.com>
2019-05-05 14:05:20 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,8 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
+        Benvolio:{name:"Benvolio",age:"17",attitude:"worried"}
+        Mercutio:{name:"Benvolio",age:"18",attitude:"hot-headed"}
       ]
    }, 
    :capulet => {
@@ -14,6 +16,8 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
+         Steven:{name:"Steven",age:"30",attitude:"confused"}
+         Nurse:{name:"Nurse",age:"18",attitude:"hot-headed"}
       ]
    }
 }
